8fa33ba9f4455a8b072f90596139f7aecd4e36fa
N-Hans <custom_karma@ukr.net>
2017-05-07 02:55:02 +0300

    Updates

--- taskServer.js
+++ taskServer.js
@@ -5,6 +5,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var port = process.env.PORT || 9090;
 app.listen(port);
 
@@ -36,7 +43,7 @@ router.get('/', function(req, res) {
 
 router.route('/tasks')
 	.post(function(req, res){
-		var task = new ITask();
+		let task = new ITask();
 		task.taskText = req.body.taskText;
 		task.isCompleted = false;
 		
@@ -54,7 +61,6 @@ router.route('/tasks')
 	})
 
 	.get(function(req, res){
-		let tasksResponse = ITask.find();
 		ITask.find(function(err, tasks){
 			if(err)
 				res.send(err);
@@ -75,12 +81,11 @@ router.route('/tasks/:task_id')
 		ITask.findById(req.params.task_id, function(err, task){
 			if(err)
 				res.send(err);
-			task.isCompleted = req.body.isCompleted;
+			task.isCompleted = req.body.isCompleted ;
 			task.taskText = req.body.taskText || task.taskText;
-			task.save(function(err,savedTask){
+			task.save(function(err){
 				if (err)
 					console.log(err);
-					res.json(err)
 				if (!err)
 					console.log("task " + task._id + " updated")
 				ITask.find(function(err, tasks){
@@ -95,11 +100,11 @@ router.route('/tasks/:task_id')
 	.delete(function(req, res){
 		ITask.remove({
 			_id: req.params.task_id
-		}, function(err, task){
+		}, function(err){
 			if(err)
 				res.send(err);
 			if(!err)
-				console.log("task " + task._id + " deleted")
+				console.log("task " + req.params.task_id + " deleted")
 			ITask.find(function(err, tasks){
 				if (err) 
 					console.log(err);
